42cb83930db632e5a09db247d701ffbbf38c1eea
Derian <dmrodriguez2000@gmail.com>
2025-01-27 08:48:31 -0600

    style(frontend): align pagination with product list

--- apps/frontend/app/marketplace/page.tsx
+++ apps/frontend/app/marketplace/page.tsx
@@ -51,7 +51,7 @@ export default function ProductList() {
 					</Button>
 				</div>
 			</section>
-			<section className="flex flex-col md:flex-row gap-6 mt-6">
+			<div className="flex flex-col md:flex-row gap-6 mt-6">
 				{/* Filters */}
 				<aside className="w-full md:w-1/4">
 					<Filters
@@ -64,62 +64,64 @@ export default function ProductList() {
 
 				{/* Product List */}
 
-				{filteredProducts.length <= 0 ? (
-					<ProductsNotFound
-						setPriceRange={setPriceRange}
-						setSelectedCategories={setSelectedCategories}
-					/>
-				) : (
-					<section className="grid flex-grow grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
-						{filteredProducts.map((product) => (
-							<Card
-								key={product.id}
-								className="hover:shadow-lg mx-auto sm:mx-0 max-w-[24rem] sm:w-auto"
-							>
-								<CardHeader>
-									<div className="aspect-square">
-										<Link
-											href={`/marketplace/${generateProductSlug(product.name)}`}
-										>
-											<Image
-												src={product.images[0].src}
-												alt={product.images[0].alt}
-												width={320}
-												height={320}
-												priority
-												className="w-full h-full rounded-t-lg cursor-pointer"
-											/>
+				<section className="flex flex-col flex-1">
+					{filteredProducts.length <= 0 ? (
+						<ProductsNotFound
+							setPriceRange={setPriceRange}
+							setSelectedCategories={setSelectedCategories}
+						/>
+					) : (
+						<div className="grid flex-grow grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+							{filteredProducts.map((product) => (
+								<Card
+									key={product.id}
+									className="hover:shadow-lg mx-auto sm:mx-0 max-w-[24rem] sm:w-auto"
+								>
+									<CardHeader>
+										<div className="aspect-square">
+											<Link
+												href={`/marketplace/${generateProductSlug(product.name)}`}
+											>
+												<Image
+													src={product.images[0].src}
+													alt={product.images[0].alt}
+													width={320}
+													height={320}
+													priority
+													className="w-full h-full rounded-t-lg cursor-pointer"
+												/>
+											</Link>
+										</div>
+										<p className="text-medium text-gray-500 px-4 pt-4">
+											{product.category}
+										</p>
+										<Link href={`/marketplace/${product.id}`}>
+											<CardTitle className="text-xl font-medium cursor-pointer hover:underline pt-0">
+												{product.name}
+											</CardTitle>
 										</Link>
-									</div>
-									<p className="text-medium text-gray-500 px-4 pt-4">
-										{product.category}
-									</p>
-									<Link href={`/marketplace/${product.id}`}>
-										<CardTitle className="text-xl font-medium cursor-pointer hover:underline pt-0">
-											{product.name}
-										</CardTitle>
-									</Link>
-								</CardHeader>
-								<CardContent className="pt-4">
-									<span className="text-3xl font-bold">${product.price}</span>
-								</CardContent>
-								<CardFooter className="flex flex-col gap-3">
-									<Button className="w-full">
-										<ShoppingCart className="mr-2 h-4 w-4 transition-transform group-hover:translate-x-1" />
-										Add to Cart
-									</Button>
-									<Button variant="secondary" className="w-full">
-										<MessageSquareMore className="mr-2 h-4 w-4" />
-										Chat with Seller
-									</Button>
-								</CardFooter>
-							</Card>
-						))}
-					</section>
-				)}
-			</section>
+									</CardHeader>
+									<CardContent className="pt-4">
+										<span className="text-3xl font-bold">${product.price}</span>
+									</CardContent>
+									<CardFooter className="flex flex-col gap-3">
+										<Button className="w-full">
+											<ShoppingCart className="mr-2 h-4 w-4 transition-transform group-hover:translate-x-1" />
+											Add to Cart
+										</Button>
+										<Button variant="secondary" className="w-full">
+											<MessageSquareMore className="mr-2 h-4 w-4" />
+											Chat with Seller
+										</Button>
+									</CardFooter>
+								</Card>
+							))}
+						</div>
+					)}
+					<ProductsPagination />
+				</section>
+			</div>
 
-			<ProductsPagination />
 			<AddProductModal isOpen={showModal} onClose={() => setShowModal(false)} />
 		</main>
 	);
